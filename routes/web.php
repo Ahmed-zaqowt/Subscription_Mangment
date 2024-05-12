e94b9a7160cf1bfe6bbfdc7923f339829c744ff3
Ahmed zaqowt <112826183+Ahmed-zaqowt@users.noreply.github.com>
2024-05-12 08:36:19 +0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -16,10 +16,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::middleware('auth')->group(function(){ 
+    
 Route::get('/' , function(){
          return view('admin.index');
 });
-Route::middleware('auth')->group(function(){
     Route::get('errors' , [ErrorController::class , 'errors'])->name('errors');
     Route::prefix('admin')->middleware('admin')->name('admin.')->group(function () {
         Route::prefix('admins')->name('admin.')->controller(\App\Http\Controllers\Admin\Admin\AdminController::class)->group(function () {
